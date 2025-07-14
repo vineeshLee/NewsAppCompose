82cbda7818134aa5b7c99f9ef576ccc2f85a83f9
vineeshlee <vineeshkumarmk25@gmail.com>
2025-07-14 16:49:01 +0530

    1.Clean architecture implementation hilt and data store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
@@ -59,4 +61,11 @@ dependencies {
 
     //Splash Api
     implementation(libs.androidx.core.splashscreen)
+
+    //appstore
+    implementation(libs.androidx.datastore.preferences)
+
+    //Dagger-hilt
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.compiler)
 }
---
+++ app/src/main/java/com/vineesh/newsapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.vineesh.newsapp.di
+
+import android.content.Context
+import com.vineesh.newsapp.NewsApplication
+import com.vineesh.newsapp.data.LoginUserManagerImpl
+import com.vineesh.newsapp.domain.manager.LoginUserManager
+import com.vineesh.newsapp.domain.usecase.IsOnBoardUseCase
+import com.vineesh.newsapp.domain.usecase.ReadIsOnBoard
+import com.vineesh.newsapp.domain.usecase.SaveIsOnBoard
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideLoginUserManager(
+        @ApplicationContext context: Context
+    ): LoginUserManager = LoginUserManagerImpl(context = context)
+
+    @Provides
+    @Singleton
+    fun provideIsOnBoardUseCase(
+        loginUserManager: LoginUserManager
+    ): IsOnBoardUseCase = IsOnBoardUseCase(
+        readIsOnBoard = ReadIsOnBoard(loginUserManager),
+        saveIsOnBoard = SaveIsOnBoard(loginUserManager)
+    )
+}
